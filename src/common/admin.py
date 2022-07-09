9cf8bf57a7ffc158eefa211601cdc619a3ad378b
Philippe GALVAN <git.philippeG@outlook.com>
2022-07-09 20:10:51 +0200

    feat: add tasks field for User in admin console

--- src/common/admin.py
+++ src/common/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from events.models import Event
+from tasks.models import Task
 from common.models import User
 
 
@@ -9,8 +10,14 @@ class EventInLine(admin.TabularInline):
     extra = 0
 
 
+class TaskInLine(admin.TabularInline):
+    model = Task.contributers.through
+    extra = 0
+
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     inlines = [
         EventInLine,
+        TaskInLine,
     ]
